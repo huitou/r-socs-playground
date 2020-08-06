75197c68700b1c227d8f21270a76283d1f8a8b4c
huitou <3.riverside.swe@gmail.com>
2020-08-06 23:01:45 +0100

    n.4 - introduce simple model play

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { object } from 'prop-types';
 
-const Index = () => {
-  return <div>hello r-socs-playground!</div>;
+import { connect } from 'r-socs-core';
+import { ObjectModel } from 'r-obj-model';
+
+const modelName = 'myObjectModel';
+const myObjStr = JSON.stringify({ addedStuff: 'addedStuff' });
+
+class TargetComponent extends Component {
+  render() {
+    return (
+      <div>
+        <div>Target Component</div>
+        <div>hifu:</div>
+        <div>{JSON.stringify(this.props[modelName].hifu)}</div>
+        <div>hefu:</div>
+        <div>
+          <button onClick={() => this.props[modelName].hefu.change(JSON.parse(myObjStr))}>change</button>
+        </div>
+        <div>
+          <button onClick={() => this.props[modelName].hefu.reset()}>reset</button>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+TargetComponent.propTypes = {
+  [modelName]: object.isRequired,
 };
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+const ServicedComponent = connect(ObjectModel, modelName)(TargetComponent);
+
+const Index = () => {
+  return (
+    <div>
+      <div>Welcome to r-socs-playground!</div>
+      <ServicedComponent initial={{ whatever: 'whatever'}}/>
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById('index'));
